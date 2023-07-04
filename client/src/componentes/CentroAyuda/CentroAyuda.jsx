6999d8eb896a2600cb9a5f04c14be02d8ac545af
Ivana-Antonella <antonellacarmona090@gmail.com>
2023-07-04 20:13:12 -0300

    componenteCentroDeAyuda

---
+++ client/src/componentes/CentroAyuda/CentroAyuda.jsx
@@ -0,0 +1,18 @@
+// import { Link } from "react-router-dom";
+import style from "./CentroAyuda.module.css";
+
+const CentroAyuda = () =>{
+
+    return(
+        <div  className={style.container}>
+         
+            <h4 >
+             Centro de Ayuda
+            </h4>
+         
+          
+        </div>
+    )
+}
+
+export default CentroAyuda;
