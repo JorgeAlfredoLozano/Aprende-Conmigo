0fe22d86fbf3f571869e36798942db5d7eb5cec2
CamCufre <cufrecamila1@gmail.com>
2023-07-04 20:28:15 -0300

    adding footer

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+// import { Link } from "react-router-dom";
+import style from "./Footer.module.css";
+
+const Footer = () =>{
+
+    return(
+        <div  className={style.container}>
+                
+         <footer className={style.nav}>
+    <p>© 2023 Mi empresa. Todos los derechos reservados.</p>
+    <p>Contacto: info@example.com</p>
+  </footer>    
+        </div>
+    )
+}
+
+export default Footer;
