b78e60538c54a618477fecc8bf5d0d4ec40fbca2
abru26 <117105228+abru26@users.noreply.github.com>
2023-07-04 15:56:15 -0300

    cambiouss

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,20 +1,21 @@
+import style from './SearchBar.module.css'
 import { useState } from "react"
 
 
 const SearchBar = ({onSearch}) => {
 
-    const [ name, setName ] = useState('')
+    const [ name, setName ] = useState('');
 
     const handleChange = (event) => {
         setName(event.target.value)
-    }
+    };
 
     return (
         <div>
          <input type='search' value={name} onChange={handleChange} />
          <button onClick={() => onSearch(name)}>Search</button>
         </div>
-    )
-}
+    );
+};
 
 export default SearchBar;
