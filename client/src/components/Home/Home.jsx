21a750c1de013f451e90c57e0d21e13195715ad0
diazarm <marceloardiaz@gmail.com>
2023-07-04 17:20:39 -0400

    con footer

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,5 +1,6 @@
  import SearchBar from "../SearchBar/SearchBar";
 import NavBar from "../NavBar/navBar";
+import Footer from "../Footer/Footer";
 
  const Home = () => {
 
@@ -8,6 +9,8 @@ import NavBar from "../NavBar/navBar";
             <NavBar/>
             <SearchBar/>
             <h1>ola soi la joum</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ab ea tempora, suscipit natus corrupti soluta rem consequuntur omnis ad pariatur sed, minus, eos ducimus esse est perspiciatis ipsum! Ex.Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ab ea tempora, suscipit natus corrupti soluta rem consequuntur omnis ad pariatur se! Ex.</p>
+            <Footer/>
         </div>
     )
  }
