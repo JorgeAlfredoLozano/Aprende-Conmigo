e2e1c3a70d00bf8352c22dcf116760786c28cf6b
Ivana-Antonella <antonellacarmona090@gmail.com>
2023-07-05 18:36:21 -0300

    avance

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './components/Home/Home';
 import SearchPage from './components/SearchPage/SearchPage';
+import Faq from './components/Faq/Faq';
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/searchPage" element={<SearchPage/>}/>
+          <Route path="/Preguntas" element={<Faq/>}/>
         </Routes>
       </div>
     </Router>
