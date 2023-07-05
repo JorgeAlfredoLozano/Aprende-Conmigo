9a891ab1f8891b7c318a342f8e5d5d8337814349
bachi9000 <126431548+bachi9000@users.noreply.github.com>
2023-07-05 09:23:35 -0400

    Delete server directory

--- server/index.js
+++
@@ -1,9 +0,0 @@
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
-
-
-conn.sync({ force: true }).then(() => {
-    server.listen(3001, () => {
-      console.log('server listening at 3001'); // eslint-disable-line no-console
-    });
-});
