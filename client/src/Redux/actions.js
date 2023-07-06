b073dbe45f9260a502b99f34f9d57ec3a51d4c02
bachi9000 <126431548+bachi9000@users.noreply.github.com>
2023-07-06 11:09:24 -0400

    fix/FormUpdate

--- client/src/Redux/actions.js
+++ client/src/Redux/actions.js
@@ -1,20 +1,20 @@
 import axios from 'axios';
 
-export function userData (user) {
+export const userData =(user)=> {
   axios.post('http://localhost:3001/user/login',  user )
     .catch(error => {
       console.error(error);
     });
 };
 
-export function putUser(payload){
+export const putUser=(payload)=>{
   return async function(dispatch){
      const response=await axios.put("ruta", payload)
-   
-     dispatch({
-      type: 'PUT_USER',
+    return dispatch({
+      type:'PUT_USER',
       payload: response
     });
+   
    }
 };
 
