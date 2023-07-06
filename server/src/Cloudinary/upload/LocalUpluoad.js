2a8e55d158945ed5a7057568ce13be78d5921e03
Ramses Garay <senkustone15@gmail.com>
2023-07-06 06:27:46 -0600

    cloudinary2

--- server/src/Cloudinary/upload/LocalUpluoad.js
+++ server/src/Cloudinary/upload/LocalUpluoad.js
@@ -7,7 +7,12 @@ console.log(cloudinary.config().api_key);
 console.log(cloudinary.config().api_secret);
 
 const imagePath = path.join(__dirname, 'Assets', 'cat.jpg')
+const imagePath2 = path.join(__dirname, 'Assets', 'banner.jpeg')
 
 cloudinary.uploader.upload(imagePath)
+	.then(result => {console.log(result)})
+	.catch(error => {console.error(error)});
+
+cloudinary.uploader.upload(imagePath2)
 	.then(result => {console.log(result)})
 	.catch(error => {console.error(error)});
