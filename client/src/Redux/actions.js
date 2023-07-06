93cf2ed8485892699217a7bc039e98bd003169d3
CamCufre <cufrecamila1@gmail.com>
2023-07-06 09:38:24 -0300

    a

--- client/src/Redux/actions.js
+++ client/src/Redux/actions.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-export default function userData (user) {
-  axios.post('http://localhost:3001/user/login',  user ) // ak va la ruta donde quieran recibir la wea
+export function userData (user) {
+  axios.post('http://localhost:3001/user/login',  user )
     .catch(error => {
       console.error(error);
     });
