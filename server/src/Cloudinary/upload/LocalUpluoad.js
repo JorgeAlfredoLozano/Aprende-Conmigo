9362a5e732e5dd64f64f21933bda298b16e8b14e
Ramses Garay <senkustone15@gmail.com>
2023-07-06 06:22:23 -0600

    cloudinary

--- server/src/Cloudinary/upload/LocalUpluoad.js
+++ server/src/Cloudinary/upload/LocalUpluoad.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
+const path = require('path');
 
 console.log(cloudinary.config().cloud_name);
 console.log(cloudinary.config().api_key);
 console.log(cloudinary.config().api_secret);
 
-cloudinary.uploader.upload("./Assets/cat.jpg")
+const imagePath = path.join(__dirname, 'Assets', 'cat.jpg')
+
+cloudinary.uploader.upload(imagePath)
 	.then(result => {console.log(result)})
 	.catch(error => {console.error(error)});
