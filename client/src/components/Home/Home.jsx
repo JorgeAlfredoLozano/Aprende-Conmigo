44d224618e852aa143b974a034064c4ef8a7c14f
CamCufre <cufrecamila1@gmail.com>
2023-07-06 20:38:49 -0300

    a

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -12,7 +12,7 @@ import style from './Home.module.css'
             <SearchBar/>
             <div className={style.container}></div>
             <div className={style.carrusel}></div>
-            <h1>ola soi la joum</h1>
+            <h1>a bachir no le gusta esta home T-T</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ab ea tempora, suscipit natus corrupti soluta rem consequuntur omnis ad pariatur sed, minus, eos ducimus esse est perspiciatis ipsum! Ex.Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ab ea tempora, suscipit natus corrupti soluta rem consequuntur omnis ad pariatur se! Ex.</p>
      
         <Footer/>
