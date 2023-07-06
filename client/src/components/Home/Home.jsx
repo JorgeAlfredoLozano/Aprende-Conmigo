9e5f73b95051feb947dcb3540bd22598b95afdd7
diazarm <marceloardiaz@gmail.com>
2023-07-06 08:20:10 -0400

    rama actualizada

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
- import SearchBar from "../SearchBar/SearchBar";
-import NavBar from "../NavBar/navBar";
-import Footer from "../Footer/Footer";
-=======
 import SearchBar from "../SearchBar/SearchBar";
 import NavBar from "../NavBar/NavBar";
 import Footer from "../Footer/Footer";
 import style from './Home.module.css'
->>>>>>> f4b98593a624d17a2ccdad1dade941f1cbcc50a5
+
 
  const Home = () => {
 
@@ -15,16 +10,12 @@ import style from './Home.module.css'
         <div>
             <NavBar/>
             <SearchBar/>
-            <div className={style.container}>
+            <div className={style.container}></div>
             <div className={style.carrusel}></div>
             <h1>ola soi la joum</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ab ea tempora, suscipit natus corrupti soluta rem consequuntur omnis ad pariatur sed, minus, eos ducimus esse est perspiciatis ipsum! Ex.Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ab ea tempora, suscipit natus corrupti soluta rem consequuntur omnis ad pariatur se! Ex.</p>
-<<<<<<< HEAD
-=======
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ab ea tempora, suscipit natus corrupti soluta rem consequuntur omnis ad pariatur sed, minus, eos ducimus esse est perspiciatis ipsum! Ex.Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ab ea tempora, suscipit natus corrupti soluta rem consequuntur omnis ad pariatur se! Ex.</p>
-            </div>
->>>>>>> f4b98593a624d17a2ccdad1dade941f1cbcc50a5
-            <Footer/>
+     
+        <Footer/>
         </div>
     )
  }
