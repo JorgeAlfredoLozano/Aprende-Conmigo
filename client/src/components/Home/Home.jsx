ca91e5ec13908530826d9b13ac5c2e077ac78144
diazarm <marceloardiaz@gmail.com>
2023-07-07 08:50:25 -0400

    carrousel actualizado

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,10 +1,6 @@
 import SearchBar from "../SearchBar/SearchBar";
 import NavBar from "../NavBar/NavBar";
-<<<<<<< HEAD
-//import Footer from "../Footer/Footer";
-=======
-// import Footer from "../Footer/Footer";
->>>>>>> 62fb2dd8bef4b5ac9825dac2254e7728be6b2e46
+import Footer from "../Footer/Footer";
 import style from './Home.module.css'
 import 'bootstrap/dist/css/bootstrap.css';
 import Carrousel from "../carrousel/Carrousel";
@@ -17,6 +13,7 @@ import Carrousel from "../carrousel/Carrousel";
             <SearchBar/>
             <div className={style.container}></div>
             <div className={style.carrusel}></div>
+            <Carrousel/>
             <h1>a bachir no le gusta esta home T-T</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ab ea tempora, suscipit natus corrupti soluta rem consequuntur omnis ad pariatur sed, minus, eos ducimus esse est perspiciatis ipsum! Ex.Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ab ea tempora, suscipit natus corrupti soluta rem consequuntur omnis ad pariatur se! Ex.</p>
         <Footer/>
