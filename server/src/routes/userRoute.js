2162fbe22e63765f5e661302b24d3caeef06754f
Ramses Garay <senkustone15@gmail.com>
2023-07-07 18:33:26 -0600

    update

--- server/src/routes/userRoute.js
+++ server/src/routes/userRoute.js
@@ -1,17 +1,9 @@
 const { Router } = require("express");
-<<<<<<< HEAD
-//const createUserHandler = require("../handlers/createUserHandler") // requerimos el handler
-=======
->>>>>>> 3e981fa5db17d68f386b5dddb851a4b1efd7513c
 const createUserLogin = require("../handlers/createUserLogin")
 const {putUser,getUser,putPhoto}=require("../rutas/User")
 const route = Router();
 
 // Define la ruta y su manejador para la creación de usuario
-<<<<<<< HEAD
-//route.post('/',createUserHandler );
-=======
->>>>>>> 3e981fa5db17d68f386b5dddb851a4b1efd7513c
 route.post('/login',createUserLogin);
 route.put('/update/:email',putUser);
 route.get('/update/:email',getUser);
