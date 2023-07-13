f8606f92500c46b5907adf41d27bb65286ab32ed
Ivana-Antonella <antonellacarmona090@gmail.com>
2023-07-13 17:03:41 -0300

    actualizada

--- client/src/components/CardsContainer/CardsContainer.jsx
+++ client/src/components/CardsContainer/CardsContainer.jsx
@@ -1,3 +1,67 @@
+// import React, { useEffect, useState } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import Card from '../Card/Card';
+// import { getAllAnuncios } from '../../Redux/actions';
+// import { NavLink } from 'react-router-dom';
+// import style from "./CardsContainer.module.css"
+
+// const CardsContainer = ({ filtro, lesson }) => {
+//   const dispatch = useDispatch();
+//   const datoPublication = useSelector((state) => state.allAnuncios);
+//   const [filteredCards, setFilteredCards] = useState([]);
+
+//   useEffect(() => {
+//     dispatch(getAllAnuncios());
+//   }, [dispatch]);
+
+//   useEffect(() => {
+//     let filteredData = datoPublication.data;
+    
+//     if (filtro) {
+//       filteredData = filteredData.filter(card => card.grade.toLowerCase().includes(filtro.toLowerCase()));
+//     }
+
+//     if (lesson) {
+//       filteredData = filteredData.filter(card => card.Lessons.some(lessonItem => lessonItem.lesson_name.toLowerCase().includes(lesson.toLowerCase())));
+      
+//     }
+
+//     setFilteredCards(filteredData);
+//   }, [filtro, lesson, datoPublication.data]);
+
+//   return (
+//     <div className={style.cardContainer}>
+//       {filteredCards &&
+//         filteredCards.map((card) =>
+//           card.status && (
+//             <div key={card.id} className={style.card_container}>
+//               <NavLink to={`/anuncio/${card.id}`} className={style.details_link}>
+//                 <Card
+//                   id={card.id}
+//                   title={card.title}
+//                   value={card.value}
+//                   lesson={card.lessons[0].lesson_name}
+//                   about_class={card.about_class}
+//                   about_teacher={card.about_teacher}
+//                   grade={card.grade}
+//                 />
+//               </NavLink>
+//             </div>
+//           )
+//         )}
+//     </div>
+//   );
+// };
+
+// export default CardsContainer;
+
+
+
+
+
+
+//______________________________________________________
+
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Card from '../Card/Card';
@@ -23,7 +87,6 @@ const CardsContainer = ({ filtro, lesson }) => {
 
     if (lesson) {
       filteredData = filteredData.filter(card => card.Lessons.some(lessonItem => lessonItem.lesson_name.toLowerCase().includes(lesson.toLowerCase())));
-      
     }
 
     setFilteredCards(filteredData);
@@ -31,25 +94,27 @@ const CardsContainer = ({ filtro, lesson }) => {
 
   return (
     <div className={style.cardContainer}>
-      {filteredCards &&
-        filteredCards.map((card) =>
-          card.status && (
-            <div key={card.id} className={style.card_container}>
-              <NavLink to={`/anuncio/${card.id}`} className={style.details_link}>
-                <Card
-                  id={card.id}
-                  title={card.title}
-                  value={card.value}
-                  lesson={card.lessons[0].lesson_name}
-                  about_class={card.about_class}
-                  about_teacher={card.about_teacher}
-                  grade={card.grade}
-                />
-              </NavLink>
-            </div>
-          )
-        )}
-    </div>
+      {filteredCards ? (
+    filteredCards.map((card) =>
+    card.status && (
+      <div key={card.id} className={style.card_container}>
+        <NavLink to={`/anuncio/${card.id}`} className={style.details_link}>
+          <Card
+            id={card.id}
+            title={card.title}
+            value={card.value}
+            lesson={card.Lessons[0].lesson_name}
+            about_class={card.about_class}
+            about_teacher={card.about_teacher}
+            grade={card.grade}
+          />
+        </NavLink>
+      </div>
+    )
+  )
+  ) : (
+  <p>No se encontraron resultados</p>)}
+  </div>
   );
 };
 
--- client/src/components/UpdatePubli/UpdatePubli.jsx
+++ client/src/components/UpdatePubli/UpdatePubli.jsx
@@ -5,30 +5,78 @@ import { useParams, useNavigate} from 'react-router-dom';
 import style from '../FormAnuncio/FormAnuncio.module.css';
 
 const UpdatePubli = (props) => {
+
+
   const dispatch=useDispatch();
   const navigate=useNavigate();
+ //me traigo el state global allPublication
   const datoPublication = useSelector((state) => state.allPublication); //Estado de todas la publicaciones
+  
   const {id}=useParams();
-  const datoId=datoPublication.data.filter((el)=>el.id===id)[0] //Avisos del usuario ID
-  console.log(datoId)
+
+  //me trae el objeto selecionado por id
+  const datoId =datoPublication.data.filter((el)=>el.id===id)[0] //Avisos del usuario ID
+  console.log("resultado del filtrado del id de publication.data =>", datoId)
+  //cuando se monta este comp quiero que de despache esta action
   useEffect(()=>{
-    dispatch(getLesson());    
+    dispatch(getLesson()); //nombre de las materias
   },[dispatch]);
 
-  const [input,setInput]=useState({ //cargo los input con los datos del estado
+  const [input,setInput]=useState({//se inicializa con los valores obtenidos del objeto filtrado datoId.
     title:datoId.title,
     about_class:datoId.about_class,
     about_teacher:datoId.about_teacher,
     value:datoId.value,
     status:datoId.status
   })
+  console.log("jiji ", input)
+//______________________________________________________
+const [errors, setErrors] = useState({});
+
   function handleChange(event) { //control de los input
         const { name, value } = event.target;
         setInput((prevState) =>({
             ...prevState,
           [name]: value,
         }))
+
+
+         // Realiza las validaciones
+  validateInput(name, value);
   }; 
+
+  function validateInput(name, value) {
+    let error = null;
+  
+    // Realiza las validaciones según el campo
+    if (name === 'title') {
+      if (!value.trim()) {
+        error = 'El título es obligatorio.';
+      }
+    } else if (name === 'about_class') {
+      if (!value.trim()) {
+        error = 'La descripción de la clase es obligatoria.';
+      }
+    } else if (name === 'about_teacher') {
+      if (!value.trim()) {
+        error = 'La descripción del profesor es obligatoria.';
+      }
+    } else if (name === 'value') {
+      if (!value.trim()) {
+        error = 'El precio es obligatorio.';
+      } else if (isNaN(value)) {
+        error = 'El precio debe ser un número.';
+      }
+    }
+  
+    // Actualiza los errores de validación
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      [name]: error,
+    }));
+  }
+  
+ //_______________________________________________________
   const handleSubmit=(event)=> { //control de botones del form Actualizar/Eliminar/Volver
         event.preventDefault();
         const boton = event.target.name
@@ -45,39 +93,56 @@ const UpdatePubli = (props) => {
             navigate('/perfil')
         };
   }
-    
-       
+  //_________________________________________________________
+
+ //___________________________________________________________      
   return (
         <div className={style.x}>
+
+
             <form className={style.container} onSubmit={(event)=>handleSubmit(event)}>
+
             <div>
                 <label className={style.label}> Título: </label>
-                <input  type="text" name='title' value={input.title} onChange={(event)=>handleChange(event)}/>                 
-             </div>              
+                <input  type="text" name='title' value={input.title} onChange={(event)=>handleChange(event)}/> 
+                {errors.title && <span className={style.error}>{errors.title}</span>}                
+            </div>        
+
             <div>
                  <label className={style.label}> Acerca De La Clase: </label> 
-                <textarea className={style.textarea} name='about_class' value={input.about_class} onChange={(event)=>handleChange(event)}/>   
-             </div>
+                <textarea className={style.textarea} name='about_class' value={input.about_class} onChange={(event)=>handleChange(event)}/> 
+                {errors.about_class && <span className={style.error}>{errors.about_class}</span>}  
+            </div>
+
             <div>
                 <label className={style.label}> Acerca Del Profesor: </label> 
-                <textarea className={style.textarea} name='about_teacher' value={input.about_teacher} onChange={(event)=>handleChange(event)}/>    
+                <textarea className={style.textarea} name='about_teacher' value={input.about_teacher} onChange={(event)=>handleChange(event)}/>  
+                {errors.about_teacher && <span className={style.error}>{errors.about_teacher}</span>}   
              </div>    
+
             <div>
                 <label className={style.label}> Precio(C/H): </label> 
-                <input type='number' name='value' value={input.value} onChange={(event)=>handleChange(event)}/>    
+                <input type='number' name='value' value={input.value} onChange={(event)=>handleChange(event)}/>  
+                {errors.value && <span className={style.error}>{errors.value}</span>} 
              </div> 
 
              <div> 
               <label > Nivel: {datoId.grade} </label>
               
             </div>
+
             <div>
               <label>Materia: {datoId.Lessons[0].lesson_name}</label>
             </div>
+
+
             <button className={style.submit} name='actualizar' type='submit' onClick={(event)=>handleSubmit(event)}>Actualizar</button>
             <button className={style.submit} name='eliminar' type='submit' onClick={(event)=>handleSubmit(event)}>Eliminar</button>
             <button className={style.submit} name='volver' type='submit' onClick={(event)=>handleSubmit(event)}>Volver</button>
+             
              </form>
+
+
         </div>
     )
 };
