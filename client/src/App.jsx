3445db61565def69dc6197e7db0069c3d5567086
CamCufre <cufrecamila1@gmail.com>
2023-07-13 21:29:26 -0300

    css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './components/Home/Home';
 import Perfil from './components/Perfil/Perfil';
