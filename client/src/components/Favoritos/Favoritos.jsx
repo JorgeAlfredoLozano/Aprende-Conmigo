73b37fcdfef9f53bb5b5ceea187ae63eff1e7597
CamCufre <cufrecamila1@gmail.com>
2023-07-15 12:47:58 -0300

    fixed perfil w/publicationUser

---
+++ client/src/components/Favoritos/Favoritos.jsx
@@ -0,0 +1,10 @@
+
+const Favoritos = () => {
+    return (
+        <>
+        <h1>Estos son los FAv</h1>
+        </>
+    )
+}
+
+export default Favoritos;
--- client/src/components/PublicationUser/PublicationUser.jsx
+++ client/src/components/PublicationUser/PublicationUser.jsx
@@ -1,3 +1,4 @@
+// PublicationUser.jsx
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import CardPublication from '../CardPublication/CardPublication';
@@ -5,14 +6,14 @@ import { getAllPublication } from '../../Redux/actions';
 import { NavLink } from 'react-router-dom';
 import style from "./Publication.module.css"
 
-const PublicationUser = () => {
+const PublicationUser = ({ submitFormAnuncio }) => {
   const dispatch = useDispatch();
   const email = localStorage.getItem('currentUser');
   const datoPublication = useSelector((state) => state.allPublication);
 
   useEffect(() => {
     dispatch(getAllPublication(email));
-  }, [dispatch]);
+  }, [dispatch, email, submitFormAnuncio]);
 
   return (
     <div className={style.publication_user_container}>
@@ -20,16 +21,16 @@ const PublicationUser = () => {
         datoPublication.data.map((card) =>
           card.status && (
             <div key={card.id} className={style.card_container}>
-               <NavLink to={`/detail/${card.id}`} className={style.details_link}>
-              <CardPublication
-                id={card.id}
-                title={card.title}
-                value={card.value}
-                lesson={card.Lessons[0].lesson_name}
-                about_class={card.about_class}
-                about_teacher={card.about_teacher}
-                grade={card.grade}
-              />
+              <NavLink to={`/detail/${card.id}`} className={style.details_link}>
+                <CardPublication
+                  id={card.id}
+                  title={card.title}
+                  value={card.value}
+                  lesson={card.Lessons[0].lesson_name}
+                  about_class={card.about_class}
+                  about_teacher={card.about_teacher}
+                  grade={card.grade}
+                />
               </NavLink>
             </div>
           )
