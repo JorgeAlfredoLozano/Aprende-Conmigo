68a3fbfbb9f09f8556f6fbce7eef72801581c7ad
bachi9000 <bachirnssr@gmail.com>
2023-07-19 10:08:52 -0400

    'close-conflicts'

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,10 +9,7 @@ import DetailAnuncio from './components/DetailAnuncio/DetailAnuncio';
 import PerfilPublic from './components/PublicPerfil/PerfilPublic';
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
-<<<<<<< HEAD
 import DashBoardAdmin from './components/DashBoardAdmin/DashBoardAdmin';
-
-=======
 import WhatsAppButton from './components/WhatsappBtn/WhatsappBtn';
 import Purchases from './components/Purchase/Purchase';
 import Sales from './components/Sales/Sales'
