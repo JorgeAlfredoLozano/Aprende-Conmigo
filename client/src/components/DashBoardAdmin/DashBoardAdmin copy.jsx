cf0fc23f1d1c3334abab804cb0e148e598ca6ea4
diazarm <marceloardiaz@gmail.com>
2023-07-20 20:15:27 -0400

    subiendo

---
+++ client/src/components/DashBoardAdmin/DashBoardAdmin copy.jsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import style from '../DashBoardAdmin/DashBoardAdmin.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+//import { getAllAnuncios } from '../../Redux/actions';
+import RestrictedAccess from "../RestrictedAccess/RestrictedAccess"
+
+
+const DashBoardAdmin = () => {
+const [admin, setAdmin] = useState(false);
+const user = localStorage.getItem('cachedUser');
+const userObject = JSON.parse(user);
+
+const dispatch = useDispatch();
+
+useEffect(() => {
+    if (userObject && userObject.admin === true) {
+      setAdmin(true);
+    } else {
+      setAdmin(false);
+    }
+}, [userObject]);
+
+    return (
+        <div>
+        {admin ? (
+            <div className={style.container}>
+            <h1>Panel de control</h1>
+            <h4>anuncios generales</h4>
+            <hr />
+
+        </div>
+        ) : (<RestrictedAccess/>) }
+      </div>  
+    );
+};
+
+export default DashBoardAdmin;
+
+    // Verificar si anuncios.data es una matriz válida antes de mapear
+    // const anuncio =
+    //     anuncios.data && Array.isArray(anuncios.data)
+    //         ? anuncios.data.map((anu) => anu.title)
+    //         : [];
+   // {(!admin) ?  <RestrictedAccess/>} 
+
+
+
+{/* Verificar si hay títulos de anuncios antes de mostrarlos */}
+{/* {anuncio.length > 0 ? (
+    <p>Anuncios: {anuncio.join(', ')}</p>
+) : (
+    <p>No hay anuncios disponibles.</p>
+)} */}
+/*
+dispatch 
+useEfect cuando se monta el compo
+importo la ruta   , [si tiene algun cambio se ejecuta]
+useSelector (para ver lo que llega)
+*/
+
---
+++ client/src/components/Login/Login copy.jsx
@@ -0,0 +1,149 @@
+import React, { useState, useEffect, useRef } from "react";
+import style from "./Login.module.css";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import firebaseConfig from "./firebaseConfig";
+import { checkUserData, getUser } from "../../Redux/actions.js";
+import { Link, useNavigate } from "react-router-dom";
+import { connect } from "react-redux";
+
+firebase.initializeApp(firebaseConfig);
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+const Login = ({ userData, getUser }) => {
+  const [logged, setLogged] = useState(false);
+  const [showLogoutButton, setShowLogoutButton] = useState(false);
+  const [currentUser, setCurrentUser] = useState(localStorage.getItem('currentUser'));
+  const [admin, setAdmin] = useState(false);
+
+  const user = localStorage.getItem('cachedUser');
+  const userObject = JSON.parse(user);
+  
+  useEffect(() => {
+    if (userObject && userObject.admin === true) {
+      setAdmin(true);
+    } else {
+      setAdmin(false);
+    }
+  }, [userObject]);
+
+  const navigate = useNavigate();
+  const panelRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (panelRef.current && !panelRef.current.contains(event.target)) {
+        setShowLogoutButton(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  useEffect(() => {
+    const cachedUser = JSON.parse(localStorage.getItem('cachedUser'));
+    if (cachedUser) {
+      setLogged(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (currentUser) {
+      setLogged(true);
+      getUser(currentUser);
+    } else {
+      setLogged(false);
+    }
+  }, [currentUser, getUser]);
+
+  useEffect(() => {
+    if (userData) {
+      localStorage.setItem('cachedUser', JSON.stringify(userData));
+    }
+  }, [userData]);
+
+  const changeDidLog = () => {
+    if (!logged) {
+      firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then((result) => {
+          const user = result.user;
+          checkUserData(user);
+          const email = user.email;
+          setCurrentUser(email);
+          setLogged(true);
+          localStorage.setItem('currentUser', email);
+          navigate('/');
+          window.location.reload()
+        })
+        .catch((error) => {
+          console.error('Error al iniciar sesión:', error);
+        });
+    } else {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          setLogged(false);
+          setShowLogoutButton(false);
+          localStorage.removeItem('currentUser');
+          localStorage.removeItem('cachedUser');         
+          navigate('/');
+          window.location.reload()
+        })
+        .catch((error) => {
+          console.error('Error al cerrar sesión:', error);
+        });
+    }
+  };
+
+  const containerStyle = {
+    backgroundImage: `url(${userData && userData.assets})`,
+  };
+
+  return (
+    <div className={style.container}>
+      {!logged && (
+        <button className={style.boton} onClick={changeDidLog}>
+          Iniciar Sesión
+        </button>
+      )}
+      {logged && (
+        <div className={style.container}>
+          <div className={style.icon} ref={panelRef} style={containerStyle} onClick={() => setShowLogoutButton(!showLogoutButton)} ></div>
+          {showLogoutButton && (
+            <div className={style.panel}>
+              <div ref={panelRef} className={style.desplegable} onClick={() => setShowLogoutButton(!showLogoutButton)} >
+                {admin && <Link to='/admin'><p className={style.botones}>Panel de Control</p></Link>}
+                <Link to='/perfil/profile'><p className={style.botones}>Mi Perfil</p></Link>
+                <Link to='/perfil/anunciosfav'><p className={style.botones}>Favoritos</p></Link>
+                <p onClick={changeDidLog} className={style.botones}>Cerrar Sesión</p>
+              </div>
+            </div>
+          )}
+          <p className={style.greet}>{userData && userData.name}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    userData: state.allInfo,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getUser: (email) => dispatch(getUser(email)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
