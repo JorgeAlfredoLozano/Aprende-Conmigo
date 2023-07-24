d18df468002976e42954af08bd8dca42a0ccfdf7
CamCufre <cufrecamila1@gmail.com>
2023-07-24 18:37:08 -0300

    aboutpage finished

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className={styles.container}>
       <div className={styles.columns}>
       <div className={styles.column}>
-          <h3>Integrantes</h3>
+          <h3 className={styles.titulo}>Integrantes</h3>
           <p><a href="https://www.linkedin.com/in/camilacufre/" target="_blank" rel="noopener noreferrer" className={styles.link}>Camila Cufré</a></p>
           <p><a href="https://www.linkedin.com/in/antonella-carmona-5a166520a/" target="_blank" rel="noopener noreferrer" className={styles.link}>Ivana Carmona</a></p>
           <p><a href="https://www.linkedin.com/in/bachir-nasser-83b1b3263/" target="_blank" rel="noopener noreferrer" className={styles.link}>Bachir Nasser</a></p>
@@ -16,19 +16,19 @@ const Footer = () => {
           <p><a href="https://www.linkedin.com/in/alejandro-garcia-6179b9260/" target="_blank" rel="noopener noreferrer" className={styles.link}>Alejandro García</a></p>
         </div>
         <div className={styles.column}>
-          <h3>Contacto</h3>
+          <h3 className={styles.titulo}>Contacto</h3>
           <a className={styles.link} href="mailto:aprendeconmigohenry@gmail.com?subject=Contacto%20desde%20mi%20sitio%20web&amp;body=Hola,%0D%0A%0D%0AQuería contactar con ustedes para obtener más información.%0D%0A%0D%0AGracias.%0D%0A%0D%0ASaludos">aprendeconmigohenry@gmail.com</a>
           <p>(+54) 0800-APRENDE</p>
         </div>
         <div className={styles.column}>
-          <h3>Acerca de Aprende Conmigo</h3>
+          <h3 className={styles.titulo}>Acerca de Aprende Conmigo</h3>
           <p><a href="/about" target="_blank" rel="noopener noreferrer" className={styles.link}>¿Quienes Somos?</a></p>
           <p>Nuestros valores</p>
           <p>Política de Privacidad</p>
         </div>
         <div className={styles.column}>
-          <h3>Asistencia</h3>
-          <a className={styles.link} href="mailto:aprendeconmigohenry@gmail.com?subject=Contacto%20desde%20mi%20sitio%20web&amp;body=Hola,%0D%0A%0D%0AQuería contactar con ustedes para obtener más información.%0D%0A%0D%0AGracias.%0D%0A%0D%0ASaludos">¿Necesitas ayuda?</a>
+          <h3 className={styles.titulo}>Asistencia</h3>
+          <p><a className={styles.link} href="mailto:aprendeconmigohenry@gmail.com?subject=Contacto%20desde%20mi%20sitio%20web&amp;body=Hola,%0D%0A%0D%0AQuería contactar con ustedes para obtener más información.%0D%0A%0D%0AGracias.%0D%0A%0D%0ASaludos">¿Necesitas ayuda?</a></p>
           <p><a href="/Preguntas" target="_blank" rel="noopener noreferrer" className={styles.link}>Preguntas Frecuentes</a></p>
         </div>
       </div>
