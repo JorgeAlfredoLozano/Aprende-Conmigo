830716460ee4705c8da924e4eb20d1ac421d66bc
“JorgeAlfredoLozano” <117942848+JorgeAlfredoLozano@users.noreply.github.com>
2023-07-28 14:17:41 -0300

    notificaciones

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -3,19 +3,26 @@ import Login from "../Login/Login";
 import { Link } from 'react-router-dom';
 import loguito from '../../assets/logo-aprende.png';
 import faqicon from '../../assets/faq-icon.png';
+import Notifications from "./Notifications";
 
-const NavBar = () =>{
-    return(         
-        <div className={style.container}>            
-            <Link to='/'>
-            <img className={style.logo} src={loguito}/>
-            </Link>
-            <Login/>
-          <Link to="/Preguntas" >
-           <img src={faqicon} className={style.faq}/>
-          </Link>
+const NavBar = () => {
+  return (
+    <div className={style.container}>
+      <Link to='/'>
+        <img className={style.logo} src={loguito} />
+      </Link>
+      <Login />
+      <div className={style.notificationContainer}>
         <Link to='/perfil/mensajes'>
+            <Notifications />
         </Link>
-        </div>
-    )}
+      </div>
+
+      <Link to="/Preguntas">
+        <img src={faqicon} className={style.faq} />
+      </Link>
+    </div>
+  );
+}
+
 export default NavBar;
--- server/src/routes/messageRoute.js
+++ server/src/routes/messageRoute.js
@@ -6,8 +6,10 @@ const getMessageByUserHandler=require('../handlers/message/getMessageByUserHandl
 const putSeenHandler = require('../handlers/message/putSeenHandler')
 const getNotReadHandler = require('../handlers/message/getNotReadHandler')
 
+
 route.post('/sendmessage',postSendMessageHandler); //Envio de mensaje
 route.get('/getallmessage/:id',getMessageByUserHandler); // traigo todos los mensajes de un usuario
 route.put('/seen/:idSend/:idReceived',putSeenHandler)
 route.get('/notread/:idSend/:idReceived',getNotReadHandler)
+
 module.exports = route;
