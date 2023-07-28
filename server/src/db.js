cb2e5a3ca4a31d095db339ab654a07ac96739da6
CamCufre <cufrecamila1@gmail.com>
2023-07-27 21:22:17 -0300

    wea

--- server/src/db.js
+++ server/src/db.js
@@ -5,11 +5,13 @@ const fs = require('fs'); // Módulo de manejo de archivos del sistema
 const path = require('path');  // Módulo para trabajar con rutas de archivos y directorios
 
 
-// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, // => localhost
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, // => localhost
+{logging: false, native: false}
+)
+
+// const sequelize = new Sequelize(DB_DEPLOY, // => localhost
 // {logging: false, native: false}
 // )
-
-https://aprender-conmig-server-production.up.railway.app/
 
 const basename = path.basename(__filename); 
 const modelDefiners = [];  
