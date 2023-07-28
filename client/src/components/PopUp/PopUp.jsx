2c7919c62921843243e6bd0f5f997a2cf2155d77
CamCufre <cufrecamila1@gmail.com>
2023-07-28 13:26:11 -0300

    popup

--- client/src/components/PopUp/PopUp.jsx
+++ client/src/components/PopUp/PopUp.jsx
@@ -1,25 +1,19 @@
 import style from './PopUp.module.css';
-// import { useNavigate } from 'react-router';
 const PopUp = ({text, setText, renderPopUp, setRenderPopUp}) => {
 
     const handleClose = () => {
-        // const navigate=useNavigate()
         const currentPath = window.location.pathname;
         setRenderPopUp(false);
         setText('');
         if (currentPath === '/perfil/usuario') {
             window.location.reload();
           }
-        //   console.log(currentPath.includes('anuncio'))
-        //    if(currentPath.includes('anuncio')){
-        //     navigate('/perfil/usuario')
-        //   }
     }
 
     return (
         <div className={style.container}>
             <div className={style.containerText}>
-                <p style={{color:"white"}}>{text}</p>
+                <p style={{color:"rgb(63, 81, 181)"}}>{text}</p>
             </div>
             <div className={style.containerButton}>
                 <button onClick={handleClose}>Volver</button>
