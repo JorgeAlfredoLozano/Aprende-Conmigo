40d77cbf36f79987159fbd244a29052e55e0f845
“JorgeAlfredoLozano” <117942848+JorgeAlfredoLozano@users.noreply.github.com>
2023-07-29 17:18:18 -0300

    paginado

--- client/src/components/Paginado/Paginado.jsx
+++ client/src/components/Paginado/Paginado.jsx
@@ -2,40 +2,51 @@ import React, { useState, useEffect } from "react";
 import styles from "./Paginado.module.css";
 
 export default function Paginado({ anunciosPerPage, allAnuncios, paginado, currentPage }) {
-  const [displayPages, setDisplayPages] = useState([]); //la paginas a mostrarse 
+  if (allAnuncios === 0) {
+    return null;
+  }
+  const [displayPages, setDisplayPages] = useState([]);
 
-  /* CALCULOS Y MANEJO DEL PAGINADO */
   useEffect(() => {
-    const totalPages = Math.ceil(allAnuncios / anunciosPerPage);
-    const maxDisplayPages = 3; //cantidad de paginas en la nav
-    let startPage = Math.max(currentPage - Math.floor(maxDisplayPages / 2), 1);
-    let endPage = Math.min(startPage + maxDisplayPages - 1, totalPages);
+    if (allAnuncios > 0) {
+      // Restricción para currentPage
+      
+      if (currentPage < 1) {
+        paginado(1);
+      } else if (currentPage > Math.ceil(allAnuncios / anunciosPerPage)) {
+        paginado(Math.ceil(allAnuncios / anunciosPerPage));
+      }
 
-    if (endPage - startPage < maxDisplayPages - 1) {
-      startPage = Math.max(endPage - maxDisplayPages + 1, 1);
-    }
+      // Restricción para displayPages
+      const totalPages = Math.ceil(allAnuncios / anunciosPerPage);
+      const maxDisplayPages = 3;
+      let startPage = Math.max(currentPage - Math.floor(maxDisplayPages / 2), 1);
+      let endPage = Math.min(startPage + maxDisplayPages - 1, totalPages);
 
-    const pages = [];
-    for (let i = startPage; i <= endPage; i++) {
-      pages.push(i);
-    }
+      if (endPage - startPage < maxDisplayPages - 1) {
+        startPage = Math.max(endPage - maxDisplayPages + 1, 1);
+      }
 
-    setDisplayPages(pages); //mostrar las paginas
+      const pages = [];
+      for (let i = startPage; i <= endPage; i++) {
+        pages.push(i);
+      }
+
+      setDisplayPages(pages);
+    } 
   }, [currentPage, allAnuncios, anunciosPerPage]);
 
   const [inputPage, setInputPage] = useState("");
   const [errorInput, setErrorInput] = useState("");
 
-  /* HANDLER DEL INPUT PARA GO TO PAGE */
   const handleInputChange = (event) => {
     setInputPage(event.target.value);
     setErrorInput("");
   };
 
-  /* HANDLER BOTON GO TO PAGE */
-  const handleGoToPage = () => { 
+  const handleGoToPage = () => {
     const pageNumber = parseInt(inputPage, 10);
-    if (pageNumber >= 1 && pageNumber <= Math.ceil(allAnuncios / anunciosPerPage)) { //controlo si es valido o tiro el error
+    if (pageNumber >= 1 && pageNumber <= Math.ceil(allAnuncios / anunciosPerPage)) {
       paginado(pageNumber);
       setInputPage("");
     } else {
@@ -43,69 +54,100 @@ export default function Paginado({ anunciosPerPage, allAnuncios, paginado, curre
     }
   };
 
-  /* HANDLER PARA CAPTURAR EL ENTER */  
   const handleKeyDown = (event) => {
     if (event.key === "Enter") {
       handleGoToPage();
     }
   };
 
-  /* HANDLER BOTON GO TO HOME EN NAV DE PAGINADO */
   const handleHomeBtn = () => {
     paginado(1);
-  }
+  };
+
+  const isFirstPage = currentPage === 1;
+  const isLastPage = currentPage === Math.ceil(allAnuncios / anunciosPerPage);
 
   return (
-    <div className={styles.container} style={{marginBottom: "15px"}}>
+    <div className={styles.container} style={{ marginBottom: "15px" }}>
       <ul className={styles.paginado} style={{ display: "flex", alignItems: "center", gap: "1em" }}>
-
         {/* BOTON HOME DE LA NAV */}
-        <li><p className={styles.inicio} onClick={handleHomeBtn}>Inicio</p></li>
-        
-        {/* BOTON ANTERIOR DE LA NAV */}
-        <li className={currentPage === 1 ? styles.disabled : ""}>
-          <p style={{cursor:"pointer", margin:"0%"}} onClick={() => paginado(currentPage - 1)} disabled={currentPage === 1}>
+        <li>
+          <p className={styles.inicio} onClick={handleHomeBtn}>
+            Inicio
+          </p>
+        </li>
+
+        <li>
+          <p
+            style={{
+              cursor: isFirstPage ? "not-allowed" : "pointer",
+              margin: "0%",
+              color: isFirstPage ? "#777171" : "inherit",
+              pointerEvents: isFirstPage ? "none" : "auto",
+            }}
+            onClick={() => {
+              if (!isFirstPage) {
+                paginado(currentPage - 1);
+              }
+            }}
+          >
             {"<"}
           </p>
-
-        {/* BOTON NUMEROS DE LA NAV */}
         </li>
+
         {displayPages.map((number) => (
           <li
             className={currentPage === number ? `${styles.number} ${styles.active}` : styles.number}
             key={number}
           >
-            <p style={{margin:"0%"}} onClick={() => paginado(number)}>{number}</p>
+            <p style={{ margin: "0%" }} onClick={() => paginado(number)}>
+              {number}
+            </p>
           </li>
         ))}
 
-        {/* BOTON SIGUIENTE DE LA NAV */}
-        <li className={currentPage === Math.ceil(allAnuncios / anunciosPerPage) ? styles.disabled : ""}>
-          <p style={{cursor:"pointer", margin:"0%"}} onClick={() => paginado(currentPage + 1)} disabled={currentPage === Math.ceil(allAnuncios / anunciosPerPage)}>
+        <li>
+          <p
+            style={{
+              cursor: isLastPage ? "not-allowed" : "pointer",
+              margin: "0%",
+              color: isLastPage ? "#777171" : "inherit",
+              pointerEvents: isLastPage ? "none" : "auto",
+            }}
+            onClick={() => {
+              if (!isLastPage) {
+                paginado(currentPage + 1);
+              }
+            }}
+          >
             {">"}
           </p>
         </li>
 
-        {/* LABEL ACTUAL/ULTIMA DE LA NAV */}
         <li>
           <p className={styles.inicioFin}>
-            {currentPage} / {Math.ceil(allAnuncios / anunciosPerPage)}
+            [{currentPage} / {Math.ceil(allAnuncios / anunciosPerPage)}]
           </p>
         </li>
-
-        {/* BOTON IR A UNA PAGINA DE LA NAV */}
       </ul>
-        <li style={{display:"flex", gap:"1em"}}>
-          <p style={{ cursor: "pointer", margin:"0%" }} onClick={handleGoToPage}>
+
+      <div style={{ display: "flex", gap: "1em" }}>
+        <p style={{ cursor: "pointer", margin: "0%" }} onClick={handleGoToPage}>
           Ir a
-          </p>
-          <input
-            type="text" value={inputPage} onChange={handleInputChange} placeholder="" style={{ width: "25px" }} onKeyDown={handleKeyDown}
-          />
-        </li>
-        <p style={{ color: "red", marginTop: "5px", height: "8px", marginLeft: "10px" }} disabled={!errorInput}>
-          {errorInput && errorInput}
         </p>
+        <input
+          type="text"
+          value={inputPage}
+          onChange={handleInputChange}
+          placeholder=""
+          style={{ width: "35px", border: "1px solid rgb(35, 128, 211)" }}
+          onKeyDown={handleKeyDown}
+        />
+      </div>
+
+      <p style={{ color: "red", marginTop: "5px", height: "8px", marginLeft: "10px" }} disabled={!errorInput}>
+        {errorInput && errorInput}
+      </p>
     </div>
   );
 }
